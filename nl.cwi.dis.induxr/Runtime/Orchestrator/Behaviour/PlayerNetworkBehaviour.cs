6bf8b4eb38bd680f027897bbc8cae05332ea30ec
Thomas Röggla <t.roggla@cwi.nl>
2025-02-12 00:14:37 +0100

    added behaviour for broadcasting player position and rotation

---
+++ nl.cwi.dis.induxr/Runtime/Orchestrator/Behaviour/PlayerNetworkBehaviour.cs
@@ -0,0 +1,81 @@
+using System;
+using UnityEngine;
+using Orchestrator.Wrapping;
+
+[System.Serializable]
+public class MovementData
+{
+    public string userId;
+    public PositionData position;
+    public RotationData rotation;
+    public float timestamp;
+}
+
+[System.Serializable]
+public class PositionData
+{
+    public float x;
+    public float y;
+    public float z;
+}
+
+[System.Serializable]
+public class RotationData
+{
+    public float x;
+    public float y;
+    public float z;
+}
+
+public class PlayerNetworkBehaviour : MonoBehaviour
+{
+    public string id = Guid.NewGuid().ToString();
+    public int updateRate = 10;
+
+    private CharacterController controller;
+    private float timer = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= 1 / updateRate) {
+            timer -= 1 / updateRate;
+            SendPositionData();
+            Debug.Log("Send position data");
+        }
+
+    }
+
+    void SendPositionData() {
+        var position = controller.transform.position;
+        var rotation = controller.transform.rotation;
+
+        var data = new MovementData()
+        {
+            userId = id,
+            timestamp = Time.time,
+            position = new PositionData()
+            {
+                x = position.x,
+                y = position.y,
+                z = position.z,
+            },
+            rotation = new RotationData()
+            {
+                x = rotation.x,
+                y = rotation.y,
+                z = rotation.z,
+            }
+        };
+
+        OrchestratorController.Instance.Broadcast("transform", JsonUtility.ToJson(data));
+    }
+}
