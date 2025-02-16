6eeb80d4641ff06153906ab6148d6986fdb582eb
Thomas Röggla <t.roggla@cwi.nl>
2025-02-16 18:40:54 +0100

    implemented SendPositionData() for AvatarNetworkBehaviour

--- nl.cwi.dis.induxr/Runtime/Orchestrator/Behaviour/AvatarNetworkBehaviour.cs
+++ nl.cwi.dis.induxr/Runtime/Orchestrator/Behaviour/AvatarNetworkBehaviour.cs
@@ -3,6 +3,21 @@ using Orchestrator.Responses;
 
 namespace Orchestrator.Behaviours
 {
+    [System.Serializable]
+    public class AvatarMovementData
+    {
+        public string userId;
+        public BoneData[] bones;
+        public float timestamp;
+    }
+
+    [System.Serializable]
+    public class BoneData {
+        public string name;
+        public PositionData position;
+        public RotationData rotation;
+    }
+
     public class AvatarNetworkBehaviour : NetworkBehaviour
     {
         private SkinnedMeshRenderer mesh;
@@ -16,7 +31,33 @@ namespace Orchestrator.Behaviours
 
         public override object SendPositionData()
         {
-            return new { };
+            var boneData = new BoneData[mesh.bones.Length];
+            int i = 0;
+
+            foreach (var bone in mesh.bones) {
+                boneData[i] = new BoneData {
+                    name = bone.name,
+                    position = new PositionData {
+                        x = bone.position.x,
+                        y = bone.position.y,
+                        z = bone.position.z
+                    },
+                    rotation = new RotationData {
+                        x = bone.rotation.x,
+                        y = bone.rotation.y,
+                        z = bone.rotation.z,
+                        w = bone.rotation.w
+                    },
+                };
+
+                i++;
+            }
+
+            return new AvatarMovementData {
+                userId = Id,
+                timestamp = Time.time,
+                bones = boneData
+            };
         }
 
         public override void OnBroadcastReceived(BroadcastData data)
