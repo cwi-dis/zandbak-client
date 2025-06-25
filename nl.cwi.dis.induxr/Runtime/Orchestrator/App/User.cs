f9c928ca91fd9b958f403d45f184d9d6c5015ee1
Thomas Röggla <t.roggla@cwi.nl>
2025-06-25 19:30:50 +0200

    added accessors for username and id to user class

--- nl.cwi.dis.induxr/Runtime/Orchestrator/App/User.cs
+++ nl.cwi.dis.induxr/Runtime/Orchestrator/App/User.cs
@@ -3,7 +3,10 @@ namespace Orchestrator.App
     public class User
     {
         private Data.User _userData;
-        public Session Session { get; set; } = null;
+
+        public Session Session { get; set; }
+        public string Id => _userData.userId;
+        public string Name => _userData.userName;
 
         public User(Data.User userData)
         {
