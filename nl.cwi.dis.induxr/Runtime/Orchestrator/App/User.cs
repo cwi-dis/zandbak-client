4fd2c0894a22bb426d90cbdd65c6199c088c20d8
Thomas Röggla <t.roggla@cwi.nl>
2025-07-18 11:44:17 +0200

    added additional properties to user class

--- nl.cwi.dis.induxr/Runtime/Orchestrator/App/User.cs
+++ nl.cwi.dis.induxr/Runtime/Orchestrator/App/User.cs
@@ -7,7 +7,22 @@ namespace Orchestrator.App
         public Session Session { get; set; }
         public string Id => _userData.Id;
         public string Name => _userData.Username;
+        /// <summary>
+        /// Indicates whether the user is currently speaking
+        /// </summary>
         public bool IsSpeaking => _userData.IsSpeaking;
+        /// <summary>
+        /// Returns the type of user (e.g. presenter, moderator, user)
+        /// </summary>
+        public string Type => _userData.UserType;
+        /// <summary>
+        /// Indicates whether the user has their hand raised currently
+        /// </summary>
+        public bool HasHandRaised => _userData.HasHandRaised;
+        /// <summary>
+        /// Returns the type of device that the user has used to connect
+        /// </summary>
+        public string DeviceType => _userData.DeviceType;
 
         public User(Data.User userData)
         {
