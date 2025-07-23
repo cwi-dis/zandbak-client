eafb1ede021befdb42962cbf8e3cdb18e2625132
Thomas Röggla <t.roggla@cwi.nl>
2025-07-23 11:56:34 +0300

    loading config from application root or folder that contains the build

--- OrchestratorSample/Assets/Scripts/ConfigLoader.cs
+++ OrchestratorSample/Assets/Scripts/ConfigLoader.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.IO;
+using Newtonsoft.Json;
 using UnityEngine;
 
 [System.Serializable]
@@ -9,37 +12,71 @@ public class AppConfig
 public class ConfigLoader : MonoBehaviour
 {
     public static AppConfig Config { get; private set; }
-    private const string ConfigFileName = "config";
+    private const string ConfigFileName = "config.json";
 
     void Awake()
     {
-        if (Config != null) return;
-
-        var configTextAsset = Resources.Load<TextAsset>(ConfigFileName);
-
-        // config.json was not found, use default values
-        if (configTextAsset == null)
+        if (Config != null)
         {
-            Debug.LogError($"Config file '{ConfigFileName}.json' not found in Resources folder.");
-
-            Config = new AppConfig
-            {
-                orchestratorUrl = "http://localhost:8090"
-            };
-
+            Debug.Log("Config already loaded.");
+            Destroy(gameObject);
             return;
         }
 
-        try
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(LoadConfig());
+    }
+
+    private IEnumerator LoadConfig()
+    {
+        var basePath = Path.GetDirectoryName(Application.dataPath);
+
+        if (Application.platform == RuntimePlatform.OSXPlayer)
         {
-            Config = JsonUtility.FromJson<AppConfig>(configTextAsset.text);
-            Debug.Log($"Loaded server address: {Config.orchestratorUrl}");
-        }
-        catch (System.Exception e)
-        {
-            Debug.LogError($"Failed to parse config file: {e.Message}");
+            basePath = Path.GetDirectoryName(basePath);
         }
 
-        DontDestroyOnLoad(gameObject);
+        if (basePath == null)
+        {
+            Debug.LogError("Failed to get base path for config file.");
+            LoadDefaultConfig();
+            yield break;
+        }
+
+        var settingsFilePath = Path.Combine(basePath, ConfigFileName);
+        Debug.Log($"Loading config from: {settingsFilePath}");
+
+        if (File.Exists(settingsFilePath))
+        {
+            try
+            {
+                var jsonText = File.ReadAllText(settingsFilePath);
+                Config = JsonConvert.DeserializeObject<AppConfig>(jsonText);
+            }
+            catch (JsonException ex)
+            {
+                Debug.LogError($"Failed to parse config JSON: {ex.Message}");
+                LoadDefaultConfig();
+                yield break;
+            }
+        }
+        else
+        {
+            Debug.LogError($"Config file not found at: {settingsFilePath}");
+            LoadDefaultConfig();
+            yield break;
+        }
+
+        Debug.Log("Config loaded from file!");
+    }
+
+    private void LoadDefaultConfig()
+    {
+        Config = new AppConfig()
+        {
+            orchestratorUrl = "http://localhost:8090",
+        };
+
+        Debug.LogWarning("Using default config.");
     }
 }
