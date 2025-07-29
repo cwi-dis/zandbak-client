a3c476cbdfcce10c3ed43369efbc47c961b237f2
Thomas Röggla <t.roggla@cwi.nl>
2025-07-29 17:36:28 +0200

    horizontally centering presentation title on text area

--- OrchestratorSample/Assets/Scripts/SessionController.cs
+++ OrchestratorSample/Assets/Scripts/SessionController.cs
@@ -284,7 +284,7 @@ public class SessionController : MonoBehaviour
 
         // Find the user object for the presenter and update text fields
         var presenterUser = _session.FindUserById(presentation.Presenter);
-        presentationInfo.text = $"<i>{presentation.Name}</i>\nby {presenterUser.Name}\n\n{presentation.CurrentSlide}\n{presentation.IsSharing}";
+        presentationInfo.text = $"<i>{presentation.Name}</i>\nby {presenterUser.Name}";
         notificationField.text += $"<i>Upcoming presentation: {presentation.Name}</i>\n";
     }
 
