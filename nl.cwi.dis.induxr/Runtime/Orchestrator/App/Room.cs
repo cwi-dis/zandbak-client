2dede5590829881e9b4a3e2d963c40e17967ad8c
Thomas Röggla <t.roggla@cwi.nl>
2026-03-25 17:27:50 +0100

    added constructor for class App.Room

--- nl.cwi.dis.induxr/Runtime/Orchestrator/App/Room.cs
+++ nl.cwi.dis.induxr/Runtime/Orchestrator/App/Room.cs
@@ -14,5 +14,11 @@ namespace Orchestrator.App
         public string Name => _roomData.Name;
         public string Description => _roomData.Description;
         public string Filename => _roomData.Filename;
+
+        public Room(Orchestrator orchestrator, Data.Room roomData)
+        {
+            _orchestrator = orchestrator;
+            _roomData = roomData;
+        }
     }
 }
