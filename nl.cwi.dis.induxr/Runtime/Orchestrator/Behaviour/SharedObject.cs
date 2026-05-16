13f4b4ed77d796e9d50d24845c94ee6b853c9821
Thomas Röggla <t.roggla@cwi.nl>
2026-05-16 02:25:26 +0200

    added beheviour which registers shared objects

---
+++ nl.cwi.dis.induxr/Runtime/Orchestrator/Behaviour/SharedObject.cs
@@ -0,0 +1,33 @@
+using Orchestrator.Util;
+using Orchestrator.Wrapping;
+using UnityEngine;
+
+namespace Orchestrator.Behaviour
+{
+    public class SharedObject : MonoBehaviour
+    {
+        private string _id;
+        private App.Orchestrator _orchestrator = OrchestratorController.Instance.Orchestrator;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        async void Start()
+        {
+            _id = StableObjectId.GetSceneObjectId(gameObject);
+            Debug.Log($"Generated object id: {_id} for gameObject {gameObject.name}");
+
+            if (!_orchestrator.CurrentSession.IsAdministrator(_orchestrator.Self))
+            {
+                return;
+            }
+
+            var sharedObject = await _orchestrator.CurrentSession.RegisterSharedObject(gameObject);
+            Debug.Log($"Registered shared object ${sharedObject.Id} for owner {sharedObject.Owner.Username} at position {sharedObject.Transform.Position}");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
