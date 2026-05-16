7e899f3bc73c88c315cd02d490b6a3f6df5bd173
Thomas Röggla <t.roggla@cwi.nl>
2026-05-16 19:51:18 +0200

    Behaviour.SharedObject now registers objects and deals with broadcasts

--- nl.cwi.dis.induxr/Runtime/Orchestrator/Behaviour/SharedObject.cs
+++ nl.cwi.dis.induxr/Runtime/Orchestrator/Behaviour/SharedObject.cs
@@ -1,3 +1,4 @@
+using Orchestrator.Data;
 using Orchestrator.Util;
 using Orchestrator.Wrapping;
 using UnityEngine;
@@ -6,28 +7,84 @@ namespace Orchestrator.Behaviour
 {
     public class SharedObject : MonoBehaviour
     {
+        [Tooltip("How many times a second the pose data should be broadcast to the server.")]
+        public int updateRate = 10;
+
         private string _id;
         private App.Orchestrator _orchestrator = OrchestratorController.Instance.Orchestrator;
+        private App.SharedObject _sharedObject;
+
+        private Rigidbody _rb;
+        private float _timer;
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         async void Start()
         {
+            _rb = GetComponent<Rigidbody>();
+
             _id = StableObjectId.GetSceneObjectId(gameObject);
             Debug.Log($"Generated object id: {_id} for gameObject {gameObject.name}");
 
-            if (!_orchestrator.CurrentSession.IsAdministrator(_orchestrator.Self))
+            if (_orchestrator.CurrentSession.IsAdministrator(_orchestrator.Self))
             {
-                return;
+                _sharedObject = await _orchestrator.CurrentSession.RegisterSharedObject(gameObject);
+                Debug.Log($"Registered shared object ${_sharedObject.Id} for owner {_sharedObject.Owner.Name} at position {_sharedObject.Position}");
             }
+            else
+            {
+                if (_rb) _rb.isKinematic = true;
 
-            var sharedObject = await _orchestrator.CurrentSession.RegisterSharedObject(gameObject);
-            Debug.Log($"Registered shared object ${sharedObject.Id} for owner {sharedObject.Owner.Name} at position {sharedObject.Position}");
+                _sharedObject = _orchestrator.CurrentSession.FindSharedObjectById(_id);
+                _sharedObject.OnObjectDataReceived += ProcessObjectUpdate;
+            }
         }
 
         // Update is called once per frame
         void Update()
         {
+            _timer += Time.deltaTime;
 
+            // Only send the transform update if the interval given by updateRate has elapsed
+            if (!(_timer >= 1f / updateRate)) return;
+            _timer -= 1f / updateRate;
+
+            // Only transmit updates if the current user owns the object
+            if (!_sharedObject.IsOwner(_orchestrator.Self)) return;
+
+            var position = gameObject.transform.position;
+            var rotation = gameObject.transform.rotation;
+
+            // Broadcast transform updates to the current session
+            _sharedObject.BroadcastUpdate(new ObjectData
+            {
+                Id = _id,
+                Timestamp = Time.time,
+                Position = new PositionData { X = position.x, Y = position.y, Z = position.z },
+                Rotation = new RotationData { X = rotation.x, Y = rotation.y, Z = rotation.z, W = rotation.w },
+            });
+        }
+
+        private void ProcessObjectUpdate(ObjectData objectData)
+        {
+            // Ignore broadcasts if the current user is the owner of the object
+            if (_sharedObject.IsOwner(_orchestrator.Self)) return;
+
+            var position = new Vector3
+            {
+                x = objectData.Position.X,
+                y = objectData.Position.X,
+                z = objectData.Position.X
+            };
+
+            var rotation = new Quaternion
+            {
+                x = objectData.Rotation.X,
+                y = objectData.Rotation.Y,
+                z = objectData.Rotation.Z,
+                w = objectData.Rotation.W
+            };
+
+            transform.SetPositionAndRotation(position, rotation);
         }
     }
 }
