b8ce536e2bb04cba3652ab469a24874a9aa66b55
Thomas Röggla <t.roggla@cwi.nl>
2026-05-16 19:50:17 +0200

    added method for sending and receiving broadcasts to App.SharedObject

--- nl.cwi.dis.induxr/Runtime/Orchestrator/App/SharedObject.cs
+++ nl.cwi.dis.induxr/Runtime/Orchestrator/App/SharedObject.cs
@@ -1,3 +1,6 @@
+using System;
+using Newtonsoft.Json;
+using Orchestrator.Data;
 using UnityEngine;
 
 namespace Orchestrator.App
@@ -11,6 +14,8 @@ namespace Orchestrator.App
 
         public string Id => _sharedObjectData.Id;
         public User Owner => Session.Users.Find((u) => u.Id == _sharedObjectData.Owner.Id);
+
+        public Action<ObjectData> OnObjectDataReceived;
 
         public Vector3 Position => new(
             _sharedObjectData.Transform.Position.X,
@@ -29,8 +34,28 @@ namespace Orchestrator.App
         {
             _orchestrator = orchestrator;
             _sharedObjectData = sharedObjectData;
+
+            _orchestrator.CurrentSession.OnBroadcastDataReceived += BroadcastReceived;
         }
 
         public bool IsOwner(User user) => _sharedObjectData.Owner.Id == user.Id;
+
+        /// <summary>
+        /// Broadcasts transform data to all users in the current session.
+        /// </summary>
+        /// <param name="data">The movement data of the avatar, including user ID, bone data, and timestamp.</param>
+        public void BroadcastUpdate(ObjectData data)
+        {
+            _orchestrator.CurrentSession?.BroadcastTransform("objectTransform", data);
+        }
+
+        private void BroadcastReceived(BroadcastData data)
+        {
+            if (data.Channel != "objectTransform") return;
+            var objectTransform = JsonConvert.DeserializeObject<ObjectData>(data.Data);
+
+            if (objectTransform.Id != Id) return;
+            OnObjectDataReceived?.Invoke(objectTransform);
+        }
     }
 }
