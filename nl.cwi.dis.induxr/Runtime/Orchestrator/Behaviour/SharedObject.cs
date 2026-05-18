43a64fd93ac433e7e592e1607706ae36fbc88d69
Thomas Röggla <t.roggla@cwi.nl>
2026-05-18 17:56:03 +0200

    added possibility to smooth movement updates to Behaviour.SharedObject

--- nl.cwi.dis.induxr/Runtime/Orchestrator/Behaviour/SharedObject.cs
+++ nl.cwi.dis.induxr/Runtime/Orchestrator/Behaviour/SharedObject.cs
@@ -10,6 +10,9 @@ namespace Orchestrator.Behaviour
     {
         [Tooltip("How many times a second the pose data should be broadcast to the server.")]
         public int updateRate = 10;
+        [Header("Smoothing options")]
+        public bool withSmoothing = true;
+        public int linearInterpolationRate = 10;
 
         private string _id;
         private App.Orchestrator _orchestrator = OrchestratorController.Instance.Orchestrator;
@@ -17,6 +20,10 @@ namespace Orchestrator.Behaviour
 
         private Rigidbody _rb;
         private float _timer;
+
+        private ObjectData _previousReceivedData;
+        private ObjectData _lastReceivedData;
+        private float _lastReceiveTime;
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         private async void Start()
@@ -94,6 +101,18 @@ namespace Orchestrator.Behaviour
             // Ignore broadcasts if the current user is the owner of the object
             if (_sharedObject.IsOwner(_orchestrator.Self)) return;
 
+            if (withSmoothing)
+            {
+                UpdateObjectWithSmoothing(objectData);
+            }
+            else
+            {
+                UpdateObject(objectData);
+            }
+        }
+
+        private void UpdateObject(ObjectData objectData)
+        {
             var position = new Vector3
             {
                 x = objectData.Position.X,
@@ -110,6 +129,33 @@ namespace Orchestrator.Behaviour
             };
 
             transform.SetPositionAndRotation(position, rotation);
+        }
+
+        private void UpdateObjectWithSmoothing(ObjectData objectData)
+        {
+            // Keep track of last received movement data for linear interpolation
+            _previousReceivedData = _lastReceivedData;
+            _lastReceivedData = objectData;
+
+            // Do nothing on the first frame
+            if (_previousReceivedData == null) return;
+
+            // Compute the value of t used in linear interpolation
+            var t = Mathf.Clamp01((Time.realtimeSinceStartup - _lastReceiveTime) / (1.0f / linearInterpolationRate));
+            _lastReceiveTime = Time.realtimeSinceStartup;
+
+            transform.SetPositionAndRotation(
+                Vector3.Lerp(
+                    new Vector3(_previousReceivedData.Position.X, _previousReceivedData.Position.Y, _previousReceivedData.Position.Z),
+                    new Vector3(_lastReceivedData.Position.X, _lastReceivedData.Position.Y, _lastReceivedData.Position.Z),
+                    t
+                ),
+                Quaternion.Slerp(
+                    new Quaternion(_previousReceivedData.Rotation.X, _previousReceivedData.Rotation.Y, _previousReceivedData.Rotation.Z, _previousReceivedData.Rotation.W),
+                    new Quaternion(_lastReceivedData.Rotation.X, _lastReceivedData.Rotation.Y, _lastReceivedData.Rotation.Z, _lastReceivedData.Rotation.W),
+                    t
+                )
+            );
         }
     }
 }
