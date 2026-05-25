194fa82966ef9c00d859ea4dee04e07c25471cef
Thomas Röggla <t.roggla@cwi.nl>
2026-05-25 20:15:40 +0200

    added behaviour for and object spawner

---
+++ nl.cwi.dis.induxr/Runtime/Orchestrator/Behaviour/Shared/ObjectSpawnerBehaviour.cs
@@ -0,0 +1,39 @@
+using Orchestrator.App;
+using Orchestrator.Wrapping;
+using UnityEngine;
+
+namespace Orchestrator.Behaviour.Shared
+{
+    public class ObjectSpawnerBehaviour : MonoBehaviour
+    {
+        private App.Orchestrator _orchestrator;
+        private Session _session;
+
+        private void Awake()
+        {
+            _orchestrator = OrchestratorController.Instance.Orchestrator;
+            _session = _orchestrator.CurrentSession;
+
+            _session.OnObjectSpawned += ObjectSpawned;
+        }
+
+        private void OnDestroy()
+        {
+            _session.OnObjectSpawned -= ObjectSpawned;
+        }
+
+        private void ObjectSpawned(SharedObject spawnedObject)
+        {
+            var prefab = Resources.Load<GameObject>($"Prefabs/{spawnedObject.PrefabName}");
+
+            if (prefab)
+            {
+                Instantiate(prefab, spawnedObject.Position, spawnedObject.Rotation);
+            }
+            else
+            {
+                Debug.LogError($"Could not find prefab {spawnedObject.PrefabName} in Resources/Prefabs");
+            }
+        }
+    }
+}
