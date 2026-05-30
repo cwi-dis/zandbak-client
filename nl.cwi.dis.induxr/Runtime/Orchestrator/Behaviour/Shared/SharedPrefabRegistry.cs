566272107cb5f48eddf4ec6273735b30395cb9ea
Thomas Röggla <t.roggla@cwi.nl>
2026-05-30 18:27:43 +0200

    added ScriptableObject representing a dynamic shared object registry

---
+++ nl.cwi.dis.induxr/Runtime/Orchestrator/Behaviour/Shared/SharedPrefabRegistry.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Orchestrator.Behaviour.Shared
+{
+    [CreateAssetMenu(fileName = "NetworkPrefabRegistry", menuName = "Networking/Shared Prefab Registry")]
+    public class SharedPrefabRegistry : ScriptableObject
+    {
+        [System.Serializable]
+        public struct PrefabRegistryEntry
+        {
+            public string prefabName;
+            public GameObject prefab;
+        }
+
+        [SerializeField]
+        public List<PrefabRegistryEntry> registryEntries = new();
+
+        private readonly Dictionary<string, PrefabRegistryEntry> _registryCache = new();
+        private bool _isInitialized = false;
+
+        public void Initialize()
+        {
+            if (_isInitialized) return;
+            _registryCache.Clear();
+
+            foreach (var entry in registryEntries)
+            {
+                if (entry.prefab == null)
+                    continue;
+
+                if (string.IsNullOrEmpty(entry.prefabName))
+                {
+                    Debug.LogWarning($"Prefab registry entry with null or empty name: {entry.prefab}");
+                    continue;
+                }
+
+                if (_registryCache.ContainsKey(entry.prefabName))
+                {
+                    Debug.LogError("Duplicate prefab registry entry for name: {entry.prefabName}");
+                    continue;
+                }
+
+                _registryCache[entry.prefabName] = entry;
+            }
+
+            _isInitialized = true;
+        }
+
+        public GameObject GetPrefab(string prefabName)
+        {
+            if (!_isInitialized)
+                Initialize();
+
+            if (_registryCache.TryGetValue(prefabName, out var entry))
+                return entry.prefab;
+
+            Debug.LogWarning($"Prefab not found in registry: {prefabName}");
+            return null;
+        }
+    }
+}
